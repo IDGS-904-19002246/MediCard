901d0431b9611f3f81ff2b08abe62d4ff2dbf516
Juan Jose J <jjjestradag@gmail.com>
2023-11-29 23:49:39 -0600

    Add HerramientasParaGraficas Nov29,2023

--- rutas/index.py
+++ rutas/index.py
@@ -7,6 +7,17 @@ from db import get_connection
 from config import db
 from datetime import datetime
 # from model import Usuarios
+
+from flask import Flask, render_template
+import plotly.express as px
+from plotly.offline import plot
+import io
+import base64
+# pip install Flask matplotlib
+# pip install pandas
+
+
+
 import sys
 sys.path.append("..")
 from modelos.M_comentarios import tbl_comentarios
@@ -39,4 +50,33 @@ def opina_post():
         
     return redirect(url_for('ind.opina'))
     
-    
+@ind.route("/dashbord")
+def dashbord():
+
+
+    data = {'Categoría 1': 20, 'Categoría 2': 35, 'Categoría 3': 15}
+
+    # Generar la gráfica de barras con Plotly
+    fig = px.bar(x=list(data.keys()), y=list(data.values()), labels={'x':'Categorías', 'y':'Valores'}, title='Gráfica de Barras')
+
+    # Guardar la gráfica en formato HTML
+    graph_html = plot(fig, output_type='div')
+
+    return render_template('index/dashbord.html', graph_html=graph_html)
+    return render_template('index/dashbord.html')
+
+
+# def generate_bar_chart(data):
+#     # Crear una gráfica de barras
+#     plt.bar(data.keys(), data.values())
+#     plt.xlabel('Categorías')
+#     plt.ylabel('Valores')
+#     plt.title('Gráfica de Barras')
+
+#     # Guardar la gráfica en un objeto BytesIO
+#     img = BytesIO()
+#     plt.savefig(img, format='png')
+#     img.seek(0)
+#     plt.close()
+
+#     return img
