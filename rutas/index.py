03167ff52fa18870b690cc5d3afdc68be6b081bd
Brandon Villalobos <brandon.djpon000@gmail.com>
2023-12-01 01:06:41 -0600

    merge

--- rutas/index.py
+++ rutas/index.py
@@ -56,17 +56,52 @@ def opina_post():
 @ind.route("/dashbord")
 def dashbord():
 
+fecha = str(datetime.now())[:10]
+    grafica7_grudo = graficasF.grafica_top7medicinas(fecha)
+    grafica7 = {G[0]: G[1] for G in grafica7_grudo}
 
-    data = {'Categoría 1': 20, 'Categoría 2': 35, 'Categoría 3': 15}
+    graficaUsuarios = [{
+        'usu':G[0],
+        'tra':G[1]
+        }for G in graficasF.grafica_top7usuarios(fecha)]
 
+    pastel_crudo = graficasF.grafica_pastelEmpresas(fecha)
+    pastel_label = [G[0] for G in pastel_crudo]
+    pastel_values = [G[1] for G in pastel_crudo]
+
+    data = {
+        'x':[1,2,3,4],
+        'y1':[1,3,5,6],
+        'y2':[1,66,6,90]
+        }
+
+    fig  = px.line(data, x ='x', y=['y1','y2'], labels={'value':'Valor','variable':'lineas'})
+    fig_html = fig.to_html(full_html = False)
+# -----------------------------------------------------------------------------------------
     # Generar la gráfica de barras con Plotly
     fig = px.bar(x=list(data.keys()), y=list(data.values()), labels={'x':'Categorías', 'y':'Valores'}, title='Gráfica de Barras')
 
+    fig = px.bar(x=list(grafica7.keys()), y=list(grafica7.values()),
+        labels={'x':'Medicamento', 'y':'Tratamientos'}, title='Top 7 Medicamentos')
     # Guardar la gráfica en formato HTML
     graph_html = plot(fig, output_type='div')
 
-    return render_template('index/dashbord.html', graph_html=graph_html)
-    return render_template('index/dashbord.html')
+    # return render_template('index/dashbord.html', graph_html=graph_html)
+    # return render_template('index/dashbord.html')
+    pastel_cocinado = px.pie(names= pastel_label, values=pastel_values,title='Empresas con más presencia')
+    pastel_html = pastel_cocinado.to_html(full_html = False)
+    # pastel_html = plot(pastel_cocinado, output_type='div')
+
+    return render_template(
+        'index/dashbord.html',
+        graph_html=graph_html,
+        usu = graficaUsuarios,
+        pastel_html = pastel_html,
+        lineas_hmtl =  fig_html
+        )
+
+
+
 
 
 # def generate_bar_chart(data):
